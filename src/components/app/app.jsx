bfcc90a5dd0cb6c41e3936d936baad043e564211
Светлана Хамматова <sveta.wing@gmail.com>
2020-06-16 15:29:15 +0500

    Добавляет propTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,12 +3,13 @@ import Main from "../main/main.jsx";
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
-  const {title, genre, releaseDate} = props;
+  const {title, genre, releaseDate, filmTitles} = props;
 
   return <Main
     title = {title}
     genre = {genre}
     releaseDate = {releaseDate}
+    filmTitles = {filmTitles}
   />;
 };
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const FILM_TITLES = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`,
+  `Show more`,
+];
+
 const descFilm = {
   title: `The Grand Budapest Hotel`,
   genre: `Drama`,
@@ -15,6 +39,7 @@ const init = () => {
         title = {descFilm.title}
         genre = {descFilm.genre}
         releaseDate = {descFilm.releaseDate}
+        filmTitles = {FILM_TITLES}
       />,
       document.querySelector(`#root`)
   );
